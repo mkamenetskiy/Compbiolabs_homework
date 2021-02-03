0038dbec9c1dc464ebcfac0220069d4d86a0715f
mkamenetskiy <rita.kamenetskiy@gmail.com>
2021-02-02 23:36:31 -0700

    Final version of Lab01 Submission

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,3 @@
-cut -f 2-4 -d , PredPreyData.csv | head > FinalPredPreyDataList.csv
-awk '{print $1; exit}' TestPredPreyData.csv >> FinalPredPreyDataList.csv
-less PredPreyData.csv
+cut -f 2-4 -d , PredPreyData.csv | awk '{print $1; exit}' > FinalPredPreyData.csv
+cut -f 2-4 -d , PredPreyData.csv | tail >> FinalPredPreyData.csv
+less FinalPredPreyData.csv
